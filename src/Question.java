6fb7faf7082cfd3d05402ca91513799fa1deff46
[Jimmy Chu] <[chuchainz@outlook.com]>
2023-10-04 11:43:57 -0700

    laptop commit

---
+++ src/Question.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Question {
+    String questions;
+    List<String> choices = new ArrayList<>();
+    List<String> answers = new ArrayList<>();
+
+    //setters to get our questions, choices, and answers
+    public void setQuestion(String question){
+        this.questions = question;
+    }
+    public void setChoices(String choice, boolean answer){}
+    public void setAnswers(String choice, boolean answer){}
+
+    //getters to get our questions, choices, and answers
+    public String getQuestion(){
+        return this.questions;
+    }
+    public List<String> getChoices(){
+        return this.choices;
+    }
+    public List<String> getAnswers(){
+        return this.answers;
+    }
+
+    //methods to show our question and choices
+    public void showQuestion(){
+        System.out.println(questions);
+    }
+    public void showChoices(){
+        for (String i : this.choices){
+            System.out.println(i);
+        }
+    }
+
+}
---
+++ src/SingleChoice.java
@@ -0,0 +1,10 @@
+public class SingleChoice extends Question{
+    public void setChoices(String choices, boolean answer){
+        //add all of our choices into the choice list
+        this.choices.add(choices);
+        //if the answer boolean is "true" then we add it to the answer list
+        if (answer){
+            this.answers.add(choices);
+        }
+    }
+}
